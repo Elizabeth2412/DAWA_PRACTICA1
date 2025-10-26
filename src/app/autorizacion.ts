804c41e7671f1e0225b23ac04d459bf10d69a86c
Elizabeth2412 <163017916+Elizabeth2412@users.noreply.github.com>
2025-10-26 10:16:16 -0500

    Validación de inicio y cierre de sesion

--- src/app/autorizacion.ts
+++ src/app/autorizacion.ts
@@ -1,9 +1,13 @@
+import { B } from '@angular/cdk/keycodes';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AutorizacionService {
+public loguedo = new BehaviorSubject<boolean>(false);
+
   constructor() {}
 
   isAuthorized(): boolean {
--- src/app/menu-horizontal/menu-horizontal.ts
+++ src/app/menu-horizontal/menu-horizontal.ts
@@ -1,19 +1,35 @@
 import { Component } from '@angular/core';
 import { AutorizacionService } from '../autorizacion';
+import { OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button'; 
 
 @Component({
   selector: 'app-menu-horizontal',
-  imports: [],
+  imports: [CommonModule, MatToolbarModule, MatIconModule, MatButtonModule    ],
   templateUrl: './menu-horizontal.html',
   styleUrl: './menu-horizontal.css'
 })
-export class MenuHorizontal  implements OnInit {
-  logueado: boolean = false;
+export class MenuHorizontal  implements OnInit{
+  logueadoCabecera: boolean = false;
+
   constructor(public autorizacionService: AutorizacionService) {}
 
   ngOnInit(): void {
-    this.logueado
+    this.autorizacionService.loguedo.subscribe((data) => {
+      this.logueadoCabecera = data;
+      console.log('Estado de logueo en cabecera:', this.logueadoCabecera);
+    });
+  }
 
+  validarUsuario(){
+    this.autorizacionService.loguedo.next(true);
+  }
+
+  cerrarSesion(){
+    this.autorizacionService.loguedo.next(false);
   }
 
 }
